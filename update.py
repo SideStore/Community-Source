01ba40596d38457ea3533e43dac60c304e2d9a70
Spidy123222 <64176728+Spidy123222@users.noreply.github.com>
2022-12-25 01:28:29 -0800

    Add play in update.py

--- update.py
+++ update.py
@@ -20,7 +20,12 @@ sourcesData = [
     {
         "parser": Parser.ALTSOURCE,
         "kwargs": {"filepath": "https://quarksources.github.io/quarksource.json"},
-        "ids": ["com.libretro.RetroArchiOS11", "com.louisanslow.record", "org.scummvm.scummvm", "com.dry05.filzaescaped11-12", "com.virtualapplications.play"]
+        "ids": [com.libretro.RetroArchiOS11", "com.louisanslow.record", "org.scummvm.scummvm", "com.dry05.filzaescaped11-12"]
+    },
+    {
+        "parser": Parser.ALTSOURCE,
+        "kwargs": {"filepath": "staticApps/play.json"},
+        "ids": ["com.virtualapplications.play"]
     },
     {
         "parser": Parser.ALTSOURCE,
